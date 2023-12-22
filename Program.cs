cb885e3f27e9b58c76c3341ed289cc234cb25aec
Thayana Fortunato <154566731+thayanaf2023@users.noreply.github.com>
2023-12-22 00:03:39 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FUNDAMENTOSCODE.Models;
+
+DateTime dataAtual = DateTime.Now.AddDays(7);
+Console.WriteLine(dataAtual);
+
+
+//string apresentacao = "Olá, seja bem vindo";
+
+
+
+//int quantidade = 1;
+//Console.WriteLine("Valor da Variavel quantidade: " + quantidade);
+
+ //quantidade = 10;
+ //Console.WriteLine("Valor da Variavel quantidade: " + quantidade);
+
+
+//double altura = 1.60;
+
+//decimal preco = 1.80m;
+
+//bool condicao = true;
+
+/////////////////////////////
+
+//Console.WriteLine(apresentacao);
+//Console.WriteLine("Valor da Variavel quantidade: " + quantidade);
+//Console.WriteLine("Valor da Variavel altura: " + altura.ToString("0.00"));
+//Console.WriteLine("Valor da Variavel preco: " + preco);
+//Console.WriteLine("Valor da Variavel condicao: " + condicao);
+
+/////////////////////////////
+
+//Pessoa1 t = new Pessoa1();
+//t.Nome = "Thayanavf";
+//t.Idade = 34;
+//t.Apresentar();
+
+
 
 
 
